ae4fa4d6def813b2e0b4c12da996ba0745ce62fc
Astamani <136489441+Astamani@users.noreply.github.com>
2024-08-29 22:58:24 +0530

    Added services and info more

--- src/app/photoservices/page.tsx
+++ src/app/photoservices/page.tsx
@@ -11,6 +11,22 @@ export const Studio_Services = () => {
                     <h2 className='text-lg md:text-4xl dark:text-white text-black mx-auto'>Pre-Wedding</h2>
                     <p className='dark:text-white text-sm md:text-base '>Capture the essence of your love story before the big day with our Pre-Wedding Photography services. Our professional photographer brings a creative touch to each session, ensuring your unique connection shines through. Enjoy beautifully edited photos that showcase your personality and romance, all at a reasonable price. Let us help you create lasting memories before you say "I do."</p>
                 </div>
+                <div className=''>
+                    <h2 className='text-lg md:text-4xl dark:text-white text-black mx-auto'>Wedding Photography</h2>
+                    <p className='dark:text-white text-sm md:text-base '>Celebrate your special day with stunning Wedding Photography that captures every precious moment. Our experienced photographer offers a blend of traditional and contemporary styles, ensuring your wedding day is documented beautifully and artistically. With high-quality editing and a commitment to excellent service, we provide exceptional value and a flawless portrayal of your celebration.</p>
+                </div>
+                <div className=''>
+                    <h2 className='text-lg md:text-4xl dark:text-white text-black mx-auto'>Kids Photography</h2>
+                    <p className='dark:text-white text-sm md:text-base '>Preserve the joy and innocence of childhood with our Kids Photography services. Our professional photographer excels in capturing the playful spirit and personality of your children, creating images that are both vibrant and memorable. Enjoy top-notch quality and creativity, all at a reasonable price, ensuring you get the best value for your precious memories.</p>
+                </div>
+                <div className=''>
+                    <h2 className='text-lg md:text-4xl dark:text-white text-black mx-auto'>Maternity Photography</h2>
+                    <p className='dark:text-white text-sm md:text-base '>Celebrate the beauty of your journey with our Maternity Photography sessions. We offer a blend of elegance and creativity to highlight the joy and anticipation of expecting a new addition to your family. Our professional photographer will guide you through poses and settings that reflect your unique experience, with high-quality editing and an affordable price that makes capturing this special time easy.</p>
+                </div>
+                <div className=''>
+                    <h2 className='text-lg md:text-4xl dark:text-white text-black mx-auto'>Indoor Photography and Outdoor Photography</h2>
+                    <p className='dark:text-white text-sm md:text-base '>Embrace the beauty of nature with our Outdoor Photography sessions. Our expert photographer utilizes natural light and scenic settings to create breathtaking photos that highlight the essence of your occasion. From stunning landscapes to urban backdrops, we deliver high-quality, artistically edited images at a price that provides outstanding value and Transform any indoor space into a stunning backdrop with our Indoor Photography services. Whether it’s for a portrait, event, or special occasion, our professional photographer uses expert techniques to make the most of your environment. Expect crisp, high-quality images and a touch of creativity, all while enjoying exceptional value and service</p>
+                </div>
             </div>
         </div>
     )
