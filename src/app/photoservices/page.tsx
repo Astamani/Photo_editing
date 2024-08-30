4569c6d6d45208586a2434eac8c9fdf33bdcbfb5
Astamani <136489441+Astamani@users.noreply.github.com>
2024-08-30 21:12:30 +0530

    error solved in deployment

--- src/app/photoservices/page.tsx
+++ src/app/photoservices/page.tsx
@@ -9,7 +9,7 @@ export const Studio_Services = () => {
             <div className='w-full h-auto antialiased flex justify-center items-center flex-col'>
                 <div className=''>
                     <h2 className='text-lg md:text-4xl dark:text-white text-black mx-auto'>Pre-Wedding</h2>
-                    <p className='dark:text-white text-sm md:text-base '>Capture the essence of your love story before the big day with our Pre-Wedding Photography services. Our professional photographer brings a creative touch to each session, ensuring your unique connection shines through. Enjoy beautifully edited photos that showcase your personality and romance, all at a reasonable price. Let us help you create lasting memories before you say &quot;I do&quot;.</p>
+                    <p className='dark:text-white text-sm md:text-base '>Capture the essence of your love story before the big day with our Pre-Wedding Photography services. Our professional photographer brings a creative touch to each session, ensuring your unique connection shines through. Enjoy beautifully edited photos that showcase your personality and romance, all at a reasonable price. Let us help you create lasting memories before you say &quot;I do&quot;</p>
                 </div>
                 <div className=''>
                     <h2 className='text-lg md:text-4xl dark:text-white text-black mx-auto'>Wedding Photography</h2>
