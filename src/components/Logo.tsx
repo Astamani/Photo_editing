25f96a59292309f4a57059d4c5e62d47c273db6f
Astamani <136489441+Astamani@users.noreply.github.com>
2024-09-17 14:31:11 +0530

    logo_1

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -4,7 +4,7 @@ const Logo = () => {
   return (
     <div className="logo-container m-0 p-0 sticky">
       <Image
-        src="/courses/logo/Phot_logo.jpg" // Replace with your logo's path
+        src="/courses/logo/Phot_logo.png" // Replace with your logo's path
         alt="Logo"
         className='m-0 p-0'
         width={300} // Adjust the width as needed
